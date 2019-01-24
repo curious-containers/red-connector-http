5e41b53605eb0634194b6b3f36d48e72d8bf2452
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2019-01-24 17:42:59 +0100

    added cli

---
+++ red_connector_http/main.py
@@ -0,0 +1,12 @@
+from cc_connector_cli.connector_cli import run_connector
+from red_connector_http.http import Http, HttpJson, HttpMockSend
+
+
+def http_main():
+    run_connector(Http)
+
+def http_json_main():
+    run_connector(HttpJson)
+
+def http_mock_send_main():
+    run_connector(HttpMockSend)
