3be5c1ed17eba3ff10a2b27780f8aad730c8f681
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2019-01-25 14:42:05 +0100

    main scripts

--- red_connector_http/main_json.py
+++ red_connector_http/main_json.py
@@ -1,10 +1,10 @@
 from cc_connector_cli.connector_cli import run_connector
-from red_connector_http.http import Http
+from red_connector_http.http import HttpJson
 from red_connector_http.version import VERSION
 
 
 def main():
-    run_connector(Http, version=VERSION)
+    run_connector(HttpJson, version=VERSION)
 
 
 if __name__ == '__main__':
