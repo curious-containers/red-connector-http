f42140e5eb6c8db91dafa6a5cc0504f5585366f5
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2019-01-25 14:23:20 +0100

    --version

--- red_connector_http/main.py
+++ red_connector_http/main.py
@@ -1,14 +1,15 @@
 from cc_connector_cli.connector_cli import run_connector
 from red_connector_http.http import Http, HttpJson, HttpMockSend
+from red_connector_http.version import VERSION
 
 
 def http_main():
-    run_connector(Http)
+    run_connector(Http, version=VERSION)
 
 
 def http_json_main():
-    run_connector(HttpJson)
+    run_connector(HttpJson, version=VERSION)
 
 
 def http_mock_send_main():
-    run_connector(HttpMockSend)
+    run_connector(HttpMockSend, version=VERSION)
